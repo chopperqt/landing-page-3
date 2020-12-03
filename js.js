2911e3a8c4641901803869e208415a0524d11b2e
Chopperxx <ilua292010@gmail.com>
2020-12-03 18:38:54 +0300

    3.3.12.20

--- js.js
+++ js.js
@@ -11,33 +11,30 @@ let line__3 = document.querySelectorAll(".line__3")[0];
 let eventContainer = false; //check open/close mobil menu. Default: false.
 
 //mobil animation styles
-let openContainer = "height: 100vh; transition: .3s";
-let closeContainer = "height: 0; transition: .3s";
+let openContainer = "height: 100vh; transition: all 0.3s ease 0s;";
+let closeContainer = "height: 0px; transition: all 0.3s ease 0s;";
 
 //open mobil menu style
 let line__1_style_open = "transition: .3s; transform: rotate(45deg) translate(5px, 5px)";
 let line__3_style_open = "transition: .3s; transform: rotate(-45deg) translate(5px, -5px)";
-let line__2_style_open = "transition: 0; opacity: 0";
+let line__2_style_open = "transition: 0s; opacity: 0";
 
 //close mobil menu style
-let line__1_style_close = "transition: .3s ;transform: rotate(0deg) translate(0, 0)";
-let line__3_style_close = "transition: .3s ;transform: rotate(0deg) translate(0, 0)";
-let line__2_style_close = "transtion: .6s; opacity: 1";
+let line__1_style_close = "transition: .3s ;transform: rotate(0deg) translate(0px, 0px)";
+let line__3_style_close = "transition: .3s ;transform: rotate(0deg) translate(0px, 0px)";
+let line__2_style_close = "transition: .6s; opacity: 1";
 
 
 
 //initilization styles
+mobilContainer.style = closeContainer;
+line__1.style = line__1_style_close;
+line__3.style = line__3_style_close;
+line__2.style = line__2_style_close;
 
-    mobilContainer.style = closeContainer;
-    line__1.style = line__1_style_close;
-    line__3.style = line__3_style_close;
-    line__2.style = line__2_style_close;
 
-
-//click listener on mobil button 
-mobilBtn.addEventListener('click' , () => {
-    console.log('click')
-    if (eventContainer) {
+function onClickBtn() {
+    if (!eventContainer) {
         mobilContainer.style = openContainer;
         line__1.style = line__1_style_open;
         line__3.style = line__3_style_open;
@@ -50,7 +47,13 @@ mobilBtn.addEventListener('click' , () => {
         line__2.style = line__2_style_close;
         eventContainer = !eventContainer;
     }
-});
+}
+
+//click listener on mobil button 
+// mobilBtn.addEventListener('click' , () => {
+//     console.log('click')
+    
+// });
 
 //transform: rotate(45deg) translate(5px, 5px);
 //transform: rotate(-45deg) translate(5px, -5px);
